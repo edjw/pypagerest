92d597387bb4a186820b74a434a3b2c7f19477a6
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2017-11-26 07:27:30 +0000

    slightly embarrassing attempts to fix setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
-#!/usr/bin/env python
+# from sys import version_info
 
-from sys import version_info
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-if version_info < (3, 4):
-    raise RuntimeError("This package requres Python 3.4+")
-
-with open('README.md', 'r', encoding='utf-8') as readme:
-    long_description = '\n' + readme.read()
+# if version_info < (3, 4):
+#     raise RuntimeError("This package requres Python 3.4+")
 
 setup(
     name='pypagerest',
     description='A Python wrapper for Page.REST',
-    long_description=long_description,
-    keywords='Page.REST scraping api Python wrapper',
+    long_description='A Python wrapper for Page.REST – an HTTP API you can use to extract content from any web page as JSON',
+    keywords='Page.REST scraping api Python wrapper async asynchronous',
     url='https://github.com/edjw/pypagerest',
     author='Ed Johnson-Williams',
     author_email='edjohnsonwilliams@gmail.com',
@@ -21,5 +20,6 @@ setup(
     license='MIT',
     packages=['pypagerest'],
     python_requires=['>=3.4'],
-    install_requires=['requests>2.18.0']
+    install_requires=['requests>=2.17.0'],
+    zip_safe=False
     )
